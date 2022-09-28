b4b235f1b541302babcf020b907c5a60b47095b1
Auxilor <william.favierparsons1@gmail.com>
2022-09-28 12:40:05 +0100

    Added custom GUI slots

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecojobs/jobs/JobLevelGUI.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecojobs/jobs/JobLevelGUI.kt
@@ -4,6 +4,7 @@ import com.willfp.eco.core.EcoPlugin
 import com.willfp.eco.core.gui.menu
 import com.willfp.eco.core.gui.menu.Menu
 import com.willfp.eco.core.gui.slot
+import com.willfp.eco.core.gui.slot.ConfigSlot
 import com.willfp.eco.core.gui.slot.FillerMask
 import com.willfp.eco.core.gui.slot.MaskItems
 import com.willfp.eco.core.items.Items
@@ -165,6 +166,7 @@ class JobLevelGUI(
                     }
                 }
             )
+
             setSlot(
                 plugin.configYml.getInt("level-gui.progression-slots.close.location.row"),
                 plugin.configYml.getInt("level-gui.progression-slots.close.location.column"),
@@ -178,6 +180,14 @@ class JobLevelGUI(
                     }
                 }
             )
+
+            for (config in plugin.configYml.getSubsections("level-gui.custom-slots")) {
+                setSlot(
+                    config.getInt("row"),
+                    config.getInt("column"),
+                    ConfigSlot(config)
+                )
+            }
         }
     }
 
--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecojobs/jobs/JobsGUI.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecojobs/jobs/JobsGUI.kt
@@ -4,6 +4,7 @@ import com.willfp.eco.core.config.updating.ConfigUpdater
 import com.willfp.eco.core.gui.menu
 import com.willfp.eco.core.gui.menu.Menu
 import com.willfp.eco.core.gui.slot
+import com.willfp.eco.core.gui.slot.ConfigSlot
 import com.willfp.eco.core.gui.slot.FillerMask
 import com.willfp.eco.core.gui.slot.MaskItems
 import com.willfp.eco.core.items.Items
@@ -197,6 +198,14 @@ object JobsGUI {
                     }
                 }
             )
+
+            for (config in plugin.configYml.getSubsections("gui.custom-slots")) {
+                setSlot(
+                    config.getInt("row"),
+                    config.getInt("column"),
+                    ConfigSlot(config)
+                )
+            }
         }
     }
 
