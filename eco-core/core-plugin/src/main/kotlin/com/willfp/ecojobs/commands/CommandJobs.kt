e212762951e2512f091664ff562d75b28a70fb60
Auxilor <william.favierparsons1@gmail.com>
2022-11-21 11:45:05 +0000

    Fix

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecojobs/commands/CommandJobs.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecojobs/commands/CommandJobs.kt
@@ -28,6 +28,10 @@ class CommandJobs(plugin: EcoPlugin) : PluginCommand(plugin, "jobs", "ecojobs.co
             return
         }
 
+        if (player.hasJob(job)) {
+            player.sendMessage(plugin.langYml.getMessage("dont-have-job"))
+        }
+
         job.levelGUI.open(player)
     }
 
